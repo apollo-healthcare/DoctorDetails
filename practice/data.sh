1032c70d269a138fb57a180826ae19aebf2bd7f5
Cloud User <ec2-user@ip-172-31-44-70.us-east-2.compute.internal>
2019-03-26 14:41:23 +0000

    code moved to Git

---
+++ practice/data.sh
@@ -0,0 +1,12 @@
+echo "Apollo HealthcareDoctors Details"
+echo "Enter Doctor Name: "
+read name
+echo "Specilist: "
+read spe
+echo "Age: "
+read age
+
+echo "Name of Doctor: "$name
+echo "Doctor Specality: "$spe
+echo "Doctor Age: "$age
+
